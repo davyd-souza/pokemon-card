e3a9a3ac3413e37c23b37dab0c7bf30458f706c1
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-02-21 15:37:57 -0300

    feat: implement apollo provider with client

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 // DEPENDENCY
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ApolloProvider } from '@apollo/client/react'
 
 // COMPONENT
 import { App } from './App'
 
+// LIB
+import { client } from './lib/apollo'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 )
